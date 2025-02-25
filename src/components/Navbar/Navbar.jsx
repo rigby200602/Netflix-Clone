d71f798b742a4c7ac78457cd70e522ee893b25cc
rigby200602 <tungle3456@gmail.com>
2025-02-25 10:56:11 +0700

    navbar-right

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
+import search_icon from '../../assets/search_icon.svg'
+import bell_icon from '../../assets/bell_icon.svg'
+import profile from '../../assets/profile_img.png'
+import caret_icon from '../../assets/caret_icon.svg'
+
+
 const Navbar = () => {
   return (
     <div className='navbars'>
@@ -16,7 +22,13 @@ const Navbar = () => {
         </ul>
       </div>
       <div className="navbar-right">
-
+        <img src={search_icon} alt="search-icon" className='icons' />
+        <p>Children</p>
+        <img src={bell_icon} alt="bell-icon" className='icons' />
+        <div className="navbar-profile">
+          <img src={profile} alt="profile" className='profile' />
+          <img src={caret_icon} alt="caret-icon" />
+        </div>
       </div>
     </div>
   )
