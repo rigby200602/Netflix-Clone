05c3c9568bcbbe192c3f11e60c54e5c05a80136c
rigby200602 <tungle3456@gmail.com>
2025-02-26 22:13:17 +0700

    navbar flex items

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,9 +10,9 @@ import caret_icon from '../../assets/caret_icon.svg'
 const Navbar = () => {
   return (
     <div className='navbar flex w-full text-sm text-[#e5e5e5] justify-between z-1'>
-      <div className="navbar-left">
-        <img src={logo} alt="Logo" />
-        <ul>
+      <div className="navbar-left flex items-center gap-[50px]">
+        <img src={logo} alt="Logo" width="90px" />
+        <ul className='flex list-none gap-[20px]'>
             <li>Home</li>
             <li>TV Shows</li>
             <li>Movies</li>
@@ -21,12 +21,12 @@ const Navbar = () => {
             <li>Browse by Languages</li>
         </ul>
       </div>
-      <div className="navbar-right">
-        <img src={search_icon} alt="search-icon" className='icons' />
+      <div className="flex gap-[20px] items-center">
+        <img src={search_icon} alt="search-icon" className='cursor-pointer w-[20px]'/>
         <p>Children</p>
-        <img src={bell_icon} alt="bell-icon" className='icons' />
-        <div className="navbar-profile">
-          <img src={profile} alt="profile" className='profile' />
+        <img src={bell_icon} alt="bell-icon" className='cursor-pointer w-[20px]' />
+        <div className="flex items-center gap-[10px] cursor-pointer">
+          <img src={profile} alt="profile" className='w-[35px] rounded-[4px]' />
           <img src={caret_icon} alt="caret-icon" />
         </div>
       </div>
