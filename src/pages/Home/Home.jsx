155d5cf287427135a802bc0e306cd0764385d1ce
rigby200602 <tungle3456@gmail.com>
2025-02-27 10:22:25 +0700

    add img

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
-import './Home' 
+import './Home.css' 
 import Navbar from '../../components/Navbar/Navbar'
+import hero_banner from '../../assets/hero_banner.jpg'
+import hero_title from '../../assets/hero_title.png'
 
 const Home = () => {
   return (
     <div className='home'>
       <Navbar/>
+      <div className="relative">
+        <img src={hero_banner} alt="background" className='banner-img w-full'/>
+        <div className="hero_caption">
+          <img src={hero_title} alt="title" />
+          <p>Discovering his ties to a secret ancient order, a young man living in modern Istanbul embarks on a quest to save the city from an immortal enemy.</p>
+        </div>
+      </div>
     </div>
   )
 }
