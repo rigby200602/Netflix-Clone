dd623473e9b73550d3a1558a989463bc3d21c462
rigby200602 <tungle3456@gmail.com>
2025-02-27 14:45:50 +0700

    add title component to home and then update css

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import './TitleCards.css'
-import data from '../../assets/cards/Cards_data'
+import cards_data from '../../assets/cards/Cards_data'
 
 const TitleCards = () => {
   return (
-    <div className='titleCards'>
-      
+    <div className='titleCards mt-[50px] mb-[30px]'>
+      <h2 className='mb-[8px]'>Popular on Netflix</h2>
+      <div className="card_list flex gap-[10px] mr-8">
+        {cards_data.map((card,index) => {
+          return <div className="card" key={index}>
+              <img src={card.image} alt="card" className='w-[240px] cursor-pointer rounded-[4px]'/>
+              <p>{card.name}</p>
+          </div>
+        }) }
+      </div>
     </div>
   )
 }
