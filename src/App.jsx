526ae14bdd30dedea1f5f46f6271bffd14cf4038
rigby200602 <tungle3456@gmail.com>
2025-03-01 16:29:09 +0700

    route path login

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Home from './pages/Home/Home'
 import { Route, Routes } from 'react-router-dom'
+import Login from '../src/pages/Login/Login'
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
       </Routes>
       <Home/>
     </div>
