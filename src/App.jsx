0e72f0cc21e142c092a5e7bb924adabc207bb1ee
rigby200602 <tungle3456@gmail.com>
2025-03-01 14:44:47 +0700

    install and import react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Home from './pages/Home/Home'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
       <Home/>
     </div>
   )
