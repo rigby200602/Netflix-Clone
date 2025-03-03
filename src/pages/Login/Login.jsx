917cb03d8d4a4762d91588c26a600ee476508728
rigby200602 <tungle3456@gmail.com>
2025-03-03 22:26:18 +0700

    login css

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -12,13 +12,15 @@ const Login = () => {
           <input type="text" placeholder='Your name' />
           <input type="text" placeholder='Email' />
           <input type="text" placeholder='Password' />
-          <button className='w-full p-[16px] bg-[#e50914] text-white rounded-[4px] text-[16px] cursor-pointer'>Sign Up</button>
+          <button className='w-full p-[16px] bg-[#e50914] text-white rounded-[4px] text-[16px] cursor-pointer font-semibold'>Sign Up</button>
           <div className="flex items-center justify-between text-[#b3b3b3] text-[13px]">
-            <div className="remember flex items-center gap-[5px]"></div>
-             <input type="checkbox"/>
-             <label htmlFor="" className='font-medium'>Remember Me</label>
-          </div>
+            <div className="remember flex items-center gap-[5px]">
+              <input className='max-w-[18px] max-h-[18px]' type="checkbox" />
+              <label>Remember Me</label>
+            </div>
             <p>Need help?</p>
+          </div>
+
         </form>
       </div>
     </div>
