de43983a81b1da45625aa03dc17e83fb1d51d9e2
rigby200602 <tungle3456@gmail.com>
2025-03-04 00:16:10 +0700

    fix button login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
           {signState === "SignUp" ? <input type="text" placeholder='Your name' /> : <></>}
           <input type="text" placeholder='Email' />
           <input type="text" placeholder='Password' />
-          <button className='w-full p-[16px] bg-[#e50914] text-white rounded-[4px] text-[16px] cursor-pointer font-semibold'>Sign Up</button>
+          <button className='w-full p-[16px] bg-[#e50914] text-white rounded-[4px] text-[16px] cursor-pointer font-semibold'>{signState}</button>
           <div className="flex items-center justify-between text-[#b3b3b3] text-[13px]">
             <div className="remember flex items-center gap-[5px]">
               <input className='max-w-[18px] max-h-[18px]' type="checkbox" />
