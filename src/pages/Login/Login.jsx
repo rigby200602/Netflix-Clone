c6769f4d537343b756efe7effa8fa6d9440a9bbb
rigby200602 <tungle3456@gmail.com>
2025-03-04 00:23:25 +0700

    switch between sign up and sign in

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
       <div className="login-form w-full max-w-[450px] rounded-[4px] p-[60px] m-auto">
         <h1 className='text-[32px] font-medium mb-[28px] text-center'>{signState}</h1>
         <form>
-          {signState === "SignUp" ? <input type="text" placeholder='Your name' /> : <></>}
+          {signState === "Sign Up" ? <input type="text" placeholder='Your name' /> : <></>}
           <input type="text" placeholder='Email' />
           <input type="text" placeholder='Password' />
           <button className='w-full p-[16px] bg-[#e50914] text-white rounded-[4px] text-[16px] cursor-pointer font-semibold'>{signState}</button>
@@ -29,8 +29,8 @@ const Login = () => {
           </div>
         </form>
         <div className="form-switch mt-[40px] text-[#737373]">
-          {signState === 'SignIn' ? <p>New to Netflix? <span>Sign Up Now</span></p> : 
-          <p>Already have account? <span>Sign In Now</span></p>}
+          {signState === 'Sign In' ? <p>New to Netflix? <span onClick={() => {setSignState('Sign Up')}}>Sign Up Now</span></p> : 
+          <p>Already have account? <span onClick={() => {setSignState('Sign In')}}>Sign In Now</span></p>}
         </div>
       </div>
     </div>
